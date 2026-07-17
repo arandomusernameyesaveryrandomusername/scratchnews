232759e01956deac36ce3decd65618a614793582
xTODB <davidas.brinzila@gmail.com>
2026-07-17 11:07:05 +0300

    cg

--- community-guidelines.php
+++ community-guidelines.php
@@ -20,17 +20,18 @@ session_start();
 </header>
 <main>
     <h2>Community Guidelines</h2>
-    <p>community guidelines so that the site doesn't go kaboom. disrespecting these guidelines will result in a ban/permanent account deletion</p>
-
-    <h3><b>1: Respect the Scratch Community Guidelines.</b></h3>
+    <p>- community guidelines so that the site doesn't go kaboom. disrespecting these guidelines will result in a ban/permanent account deletion, or a warning</p>
+    <h3><b>1 - The most important rule of all...</b></h3>
+    <h3><b>1.1: Respect the Scratch Community Guidelines.</b></h3>
     <p>Every action that you make on this has to not violate any of the Scratch Community Guidelines. <a href="https://scratch.mit.edu/community_guidelines">(seen here.)</a><br>
     This includes comments, replies and articles.</p><br>
     <i>ScratchNews is a site about Scratch-related news; therefore, Scratchers will use it; therefore, Scratch <b>requires</b> the website to make the website as safe as Scratch itself or safer, and if ScratchNews isn't safer than Scratch, then say goodbye to the idea of Scratchers using it.</i><br>
-    <h3><b>2: Do not social spam.</b></h3>
-    <p>Feels so tempting, doesn't it? Why not make another account, like your own article and get the numbers up and pumping? <b>NO. JUST NO.</b></p><br>
-    <i>Social spamming takes minimal effort, puts your article further in the search, but ruins the experience for everyone. Nobody wants an article with a million likes to show up whenever they're searching literally ANYTHING!</i><br>
-    
-    <p>these are all the rules now go enjoy the website</p>
+    <h3><b>2 - What articles you can and can't submit</b></h3>
+    <h3><b>2.1: No AI text or images.</b></h3>
+    <p>Pretty self-explanatory.</p><br>
+    <h3><b>2.2: No self-promotion</b></h3>
+    <p>Promote <i>yourself,</i> not what you made. If everyone were to self-promote, this website might as well be the Show And Tell forum 2.0.</p><br>
+    <p><b>And that's all!</b> Have fun on ScratchNews!</p>
 </main>
 <?php include __DIR__ . '/includes/footer.php'; ?>
 </body>
