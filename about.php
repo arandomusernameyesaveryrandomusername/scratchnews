813a77b54fd04004ca1d717119469476521d6137
xTODB <davidas.brinzila@gmail.com>
2026-07-18 14:16:29 +0300

    a

--- about.php
+++ about.php
@@ -21,7 +21,7 @@ session_start();
 <main>
     <h2>About</h2>
     <p>ScratchNews is a news platform about Scratch-related news, made to prevent misinformation and disinformation among the Scratch community.
-        It transmits information in the form of articles, has many social features and <br>created by Scratchers, for Scratchers.</br>
+        It transmits information in the form of articles, has many social features and created by Scratchers, for Scratchers.</br>
         ScratchNews is a growing platform, and features are added almost daily. We'd like if there were users, and users who submit articles to help our goal of making the Scratch community a little more informed every day.
     </p>
 </main>
