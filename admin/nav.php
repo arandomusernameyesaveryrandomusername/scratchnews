3260c4fedc3dfb12d440086b493e755146486a26
xTODB <davidas.brinzila@gmail.com>
2026-07-21 09:32:46 +0300

    a

--- admin/nav.php
+++ admin/nav.php
@@ -13,6 +13,7 @@
             <a href="/admin/reports.php">Reports</a>
             <a href="/admin/move.php">Move Content</a>
             <a href="/admin/impersonation-log.php">Impersonation Log</a>
+            <a href="/admin/stats.php">Stats</a>
             <a href="/logout">Log Out</a>
         </div>
     </nav>
