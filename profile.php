5cba51a0e25313dc1382c8c8a1755c3cee63b72c
xTODB <davidas.brinzila@gmail.com>
2026-07-22 22:07:35 +0300

    a

--- profile.php
+++ profile.php
@@ -29,23 +29,7 @@ $comments = $user ? getCommentsByUser($user['id']) : [];
 <link rel="stylesheet" href="/assets/style.css?v=8">
 </head>
 <body class="<?= !empty($_SESSION['dark_mode']) ? 'dark' : '' ?>">
-<header>
-    <a href="/" class="logo-link">
-<svg viewBox="0,0,136.90609,31.33279" class="logo-svg" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-172.33195,-164.3336)"><g stroke-miterlimit="10"><text transform="translate(217.16808,185.69599) scale(0.5,0.5)" font-size="40" fill="#ffffff" stroke="#ffaa33" stroke-width="3" font-family="Scratch" font-weight="normal" text-anchor="start"><tspan x="0" dy="0">ScratchNews</tspan></text><text transform="translate(217.16808,185.69599) scale(0.5,0.5)" font-size="40" fill="#ffffff" stroke="none" stroke-width="1" font-family="Scratch" font-weight="normal" text-anchor="start"><tspan x="0" dy="0">ScratchNews</tspan></text><path d="M181.04509,195.64879h-8.71313v-10.6397h8.71313z" fill="#cc8829" stroke="none"/><path d="M176.88045,195.6664v-20.46677l3.90302,-0.07587l0.09189,-5.0222h6.64479v20.71239l-3.91923,0.16783l-0.04923,4.68462z" fill="#ffaa33" stroke="none"/><path d="M201.40189,164.35122h8.71313v10.6397h-8.71313z" fill="#cc8829" stroke="none"/><path d="M205.56653,164.33361v20.46677l-3.90302,0.07587l-0.09189,5.0222h-6.64479v-20.71239l3.91923,-0.16783l0.04923,-4.68462z" fill="#ffaa33" stroke="none"/><path d="M190.06459,189.91166l-0.03808,-3.62362l-3.03158,-0.12982v-16.02128h5.13983l0.07108,3.88473l3.01904,0.05869v15.8313z" fill="#ffaa33" stroke="none"/></g></g></svg>
-    </a>
-    <nav>
-    <?php if (!empty($_SESSION['reader_username'])): ?>
-        <span>Hi, <?= e($_SESSION['reader_username']) ?>!</span>
-        <?php if (!empty($_SESSION['is_admin'])): ?>
-        <a href="/admin/">Admin</a>
-        <?php endif; ?>
-        <a href="/logout">Log Out</a>
-    <?php else: ?>
-        <a href="/login">Log In</a>
-        <a href="/register">Sign Up</a>
-    <?php endif; ?>
-    </nav>
-</header>
+<?php include __DIR__ . '/includes/header.php'; ?>
 <main>
 <?php if (!$user): ?>
     <h2>User Not Found</h2>
