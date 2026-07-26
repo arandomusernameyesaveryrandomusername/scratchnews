107df2e487a55e52ed85af79cea8f6ccc6ab0d73
xTODB <davidas.brinzila@gmail.com>
2026-07-26 18:11:46 +0300

    A

--- includes/header.php
+++ includes/header.php
@@ -10,9 +10,6 @@
     </div>
 <form method="get" action="/search" class="search-form">
     <input type="text" name="q" placeholder="Search articles...">
-    <button type="submit" aria-label="Search">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 1 0-.7.7l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0A4.5 4.5 0 1 1 14 9.5 4.5 4.5 0 0 1 9.5 14z"/></svg>
-    </button>
 </form>
 <nav>
     <?php if (!empty($_SESSION['reader_username'])): ?>
