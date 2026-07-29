f658698d3658eaa71ba234da754e591cd18bdd77
xTODB <davidas.brinzila@gmail.com>
2026-07-29 09:09:00 +0300

    a

--- includes/footer.php
+++ includes/footer.php
@@ -4,21 +4,28 @@
         <div class="footer-logo">
 <svg viewBox="0,0,136.90609,31.33279" class="footer-logo-svg" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-172.33196,-164.33361)"><g stroke-miterlimit="10"><text transform="translate(217.16809,185.696) scale(0.5,0.5)" font-size="40" fill="#ffffff" stroke="#ffaa33" stroke-width="3" font-family="Scratch" font-weight="normal" text-anchor="start"><tspan x="0" dy="0">ScratchNews</tspan></text><text transform="translate(217.16809,185.696) scale(0.5,0.5)" font-size="40" fill="#ffffff" stroke="none" stroke-width="1" font-family="Scratch" font-weight="normal" text-anchor="start"><tspan x="0" dy="0">ScratchNews</tspan></text><path d="M181.04509,195.6488h-8.71313v-10.6397h8.71313z" fill="#cc8829" stroke="none"/><path d="M176.88046,195.66641v-20.46677l3.90302,-0.07587l0.09189,-5.0222h6.64479v20.71239l-3.91923,0.16783l-0.04923,4.68462z" fill="#ffaa33" stroke="none"/><path d="M201.4019,164.35123h8.71313v10.6397h-8.71313z" fill="#cc8829" stroke="none"/><path d="M205.56654,164.33362v20.46677l-3.90302,0.07587l-0.09189,5.0222h-6.64479v-20.71239l3.91923,-0.16783l0.04923,-4.68462z" fill="#ffaa33" stroke="none"/><path d="M190.0646,189.91167l-0.03808,-3.62362l-3.03158,-0.12982v-16.02128h5.13983l0.07108,3.88473l3.01904,0.05869v15.8313z" fill="#ffaa33" stroke="none"/></g></g></svg>
         </div>
-        <div class="footer-col">
-            <h4>Info</h4>
-            <a href="/about.php">About</a>
-            <a href="/changelog.php">Changelog</a>
-            <a href="/community-guidelines.php">Community Guidelines</a>
-        </div>
         <div class="footer-columns">
-        <div class="footer-col">
-            <h4>Misc</h4>
-            <a href="/stats.php">Stats</a>
-            <a href="/api.php">API</a>
-            <a href="https://discord.com/oauth2/authorize?client_id=1531385304978817166">Discord Bot</a>
-            <a href="/feedback.php">Feedback</a>
-            <?php if (!empty($_SESSION['reader_username'])): ?><a href="/delete-account">Delete Account</a><?php endif; ?>
-            <a href="/random-article.php">Random Article</a>
+            <div class="footer-col">
+                <h4>Info</h4>
+                <a href="/about.php">About</a>
+                <a href="/changelog.php">Changelog</a>
+                <a href="/community-guidelines.php">Community Guidelines</a>
+            </div>
+            <div class="footer-col">
+                <h4>Developers</h4>
+                <a href="/stats.php">Stats</a>
+                <a href="/api.php">API</a>
+                <a href="https://discord.com/oauth2/authorize?client_id=1531385304978817166">Discord Bot</a>
+            </div>
+            <div class="footer-col">
+                <h4>Account</h4>
+                <a href="/feedback.php">Feedback</a>
+                <?php if (!empty($_SESSION['reader_username'])): ?><a href="/delete-account">Delete Account</a><?php endif; ?>
+            </div>
+            <div class="footer-col">
+                <h4>More</h4>
+                <a href="/random-article.php">Random Article</a>
+            </div>
         </div>
     </div>
     <div class="footer-copyright">&copy; <?= e(SITE_NAME) ?> v<?= e(SITE_VERSION) ?></div>
